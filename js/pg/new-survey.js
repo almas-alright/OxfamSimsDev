27ea1107d906bb67ffa2e9b49c3e9ce5244f5ccd
almas-alright <kutsnalmas@gmail.com>
2016-03-10 16:41:40 +0600

    btn test

--- js/pg/new-survey.js
+++ js/pg/new-survey.js
@@ -1,134 +1,84 @@
-/* 
- * syurvey data processing
- */
-
-
-
-//function getLocationPos() {
-//    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-//}
-//// onSuccess Geolocation  
-//function onSuccess(position) {
-//    document.getElementById('lat').value = position.coords.latitude;
-//    document.getElementById('lon').value = position.coords.longitude;
-//}
-//function onError(error) {
-//    alert('code: ' + error.code + '\n' +
-//            'message: ' + error.message + '\n');
-//}
-
-
-//$(document).bind('deviceready', function () {
-//    
-//    $("#benificiarySDcardBtn").bind('touchstart', function () {
-//        alert("working");
-//    })
-//   
-//        $("#benificiaryPhotoBtn").bind('touchstart', function () {
-//            navigator.camera.getPicture(
-//                    function (data) {
-//                        $("#benificiaryPhoto").attr('src', "data:image/jpeg;base64," + data).css("visibility", "visible");
-//                    },
-//                    onCaptureFail,
-//                    {                        
-//                        destinationType: destinationType.DATA_URL,
-//                        sourceType: PictureSourceType.PHOTOLIBRARY,                       
-//                        mediaType: MediaType.PICTURE
-//                    }
-//            );
-//        });   
-//
-//    function onCaptureFail(message) {
-//        alert('Failed because: ' + message);
-//    }
-//});
-
-
 var pictureSource;   // picture source
-var destinationType; // sets the format of returned value
+    var destinationType; // sets the format of returned value
 
-// Wait for device API libraries to load
-//
-document.addEventListener("deviceready", onDeviceReady, false);
-
-// device APIs are available
-//
-function onDeviceReady() {
-    pictureSource = navigator.camera.PictureSourceType;
-    destinationType = navigator.camera.DestinationType;
-}
-
-// Called when a photo is successfully retrieved
-//
-function onPhotoDataSuccess(imageData) {
-    // Uncomment to view the base64-encoded image data
-    // console.log(imageData);
-
-    // Get image handle
+    // Wait for device API libraries to load
     //
-    var smallImage = document.getElementById('benificiaryPhoto');
+    document.addEventListener("deviceready",onDeviceReady,false);
 
-    // Unhide image elements
+    // device APIs are available
     //
-    smallImage.style.display = 'block';
+    function onDeviceReady() {
+        pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
+    }
 
-    // Show the captured photo
-    // The in-line CSS rules are used to resize the image
+    // Called when a photo is successfully retrieved
     //
-    smallImage.src = "data:image/jpeg;base64," + imageData;
-}
+    function onPhotoDataSuccess(imageData) {
+      // Uncomment to view the base64-encoded image data
+      // console.log(imageData);
 
-// Called when a photo is successfully retrieved
-//
-function onPhotoURISuccess(imageURI) {
-    // Uncomment to view the image file URI
-    // console.log(imageURI);
+      // Get image handle
+      //
+      var smallImage = document.getElementById('benificiaryPhoto');
 
-    // Get image handle
+      // Unhide image elements
+      //
+      smallImage.style.display = 'block';
+
+      // Show the captured photo
+      // The in-line CSS rules are used to resize the image
+      //
+      smallImage.src = "data:image/jpeg;base64," + imageData;
+    }
+
+    // Called when a photo is successfully retrieved
     //
-    var largeImage = document.getElementById('benificiaryPhoto');
+    function onPhotoURISuccess(imageURI) {
+      // Uncomment to view the image file URI
+      // console.log(imageURI);
 
-    // Unhide image elements
+      // Get image handle
+      //
+      var largeImage = document.getElementById('benificiaryPhoto');
+
+      // Unhide image elements
+      //
+      largeImage.style.display = 'block';
+
+      // Show the captured photo
+      // The in-line CSS rules are used to resize the image
+      //
+      largeImage.src = imageURI;
+    }
+
+    // A button will call this function
     //
-    largeImage.style.display = 'block';
+    function capturePhoto() {
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+        destinationType: destinationType.DATA_URL });
+    }
 
-    // Show the captured photo
-    // The in-line CSS rules are used to resize the image
+    // A button will call this function
     //
-    largeImage.src = imageURI;
-}
+    function capturePhotoEdit() {
+      // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+        destinationType: destinationType.DATA_URL });
+    }
 
-// A button will call this function
-//
-function capturePhoto() {
-    // Take picture using device camera and retrieve image as base64-encoded string
-    navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 50,
-        destinationType: destinationType.DATA_URL});
-}
-
-// A button will call this function
-//
-function capturePhotoEdit() {
-    // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
-    navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 20, allowEdit: true,
-        destinationType: destinationType.DATA_URL});
-}
-
-// A button will call this function
-//
-function getPhoto(source) {
-    // Retrieve image file location from specified source
-    navigator.camera.getPicture(onPhotoURISuccess, onFail, {quality: 50,
+    // A button will call this function
+    //
+    function getPhoto(source) {
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
         destinationType: destinationType.FILE_URI,
-        sourceType: source});
-}
+        sourceType: source });
+    }
 
-// Called if something bad happens.
-//
-function onFail(message) {
-    alert('Failed because: ' + message);
-}
-
-
-
-
+    // Called if something bad happens.
+    //
+    function onFail(message) {
+      alert('Failed because: ' + message);
+    }
