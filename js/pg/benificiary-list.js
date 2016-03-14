1dc5274834cf89b58cb8aaebffd99e8f33ada349
almas-alright <kutsnalmas@gmail.com>
2016-03-14 18:19:19 +0600

    list

---
+++ js/pg/benificiary-list.js
@@ -0,0 +1,29 @@
+
+
+function errorCB(tx, err) {
+    alert("Error processing SQL: " + err);
+}
+
+// Transaction success callback
+//
+function successCB() {
+    alert("success!");
+}
+
+
+function benificiaryList(tx, results) {
+    var benf_single = '';
+    var len = results.rows.length;
+    for (var i = 0; i < len; i++) {
+        benf_single += '<li class="list-group-item"><img class="text-center" src="' + results.rows.item(i).benificiary_img + '" alt=""> <span>Name: ' + results.rows.item(i).benificiary_name + '</span></li>';
+    }
+    $('#beneficiary-list').html(benf_single);
+}
+
+function populateBeneficiaryList() {
+    var db = window.openDatabase("oxfam_sims_dev", "1.0", "OxfamSIMS", 1000000);
+    db.transaction(function (tx) {
+        tx.executeSql('SELECT * FROM beneficiary_info ORDER BY b_id DESC', [], benificiaryList, errorCB);
+    }, errorCB, successCB);
+
+}
--- js/pg/new-survey.js
+++ js/pg/new-survey.js
@@ -1,15 +1,41 @@
 
 
 $(document).ready(function () {
-        populateBeneficiaryList();
+    populateBeneficiaryList();
     $('#save').on('click', function () {
+        var select_id = get_input_value_of("#select_id"); 
+        var response_by = get_input_value_of("#response_by"); 
+        var benificiary_name = get_input_value_of("#beneficiary_name");
+        var benificiary_img = $("#benificiaryPhoto").attr("src");
+        var fathers_name = get_input_value_of("#fathers_name");
+        var mothers_name = get_input_value_of("#mothers_name");
+        var union_name = get_input_value_of("#union_name");
+        var word_name = get_input_value_of("#word_name");
+        var address = get_input_value_of("#address");
+        var gender = get_input_value_of("#gender");
+        var age = get_input_value_of("#age");
+        var mobile_no = get_input_value_of("#mobile_no");
+        var nominee_name = get_input_value_of("#nominee_name");
+        var relation_with_nom = get_input_value_of("#relation_with_nom");
+        var nominee_img = $("#nominee_img").attr("src");
+        var marital_status = $("input:radio[name=married]").val();
+        var occupation_main = get_input_value_of("#occupation_main");
+        var occupation_2 = get_input_value_of("#occupation_2");
+        var occupation_3 = get_input_value_of("#occupation_3");
+        var location_gps = get_input_value_of("#lat")+","+get_input_value_of("#lon");
+
+        //word, address, grnder, age, mobile, nominee_name, relation, nominee_img, marital_sts, occupation, occupation_1, occupation_2, location_gps
         var db = window.openDatabase("oxfam_sims_dev", "1.0", "OxfamSIMS", 1000000);
         db.transaction(function (tx) {
-           tx.executeSql('INSERT INTO beneficiary_info (select_id, response_by, benificiary_name, benificiary_img, fathers_name, mothers_name, union_name, word, address, grnder, age, mobile, nominee_name, relation, nominee_img, marital_sts, occupation, occupation_1, occupation_2, location_gps) VALUES ("1234", "response by people", "benificiary had name", "img/img/img/benf.jpg", "father", "mother", "union intersect", "word no 152", "no address", "female", "16 yrs", "01678591535", "nominee has name", "boy friend", "img/img/img/nominee.jpg", "unmaried", "hair dresser", "begger", "sex worker", "23.737785, 90.395173")');
+            tx.executeSql('INSERT INTO beneficiary_info (select_id, response_by, benificiary_name, benificiary_img, fathers_name, mothers_name, union_name, word, address, grnder, age, mobile, nominee_name, relation, nominee_img, marital_sts, occupation, occupation_1, occupation_2, location_gps) VALUES ("'+select_id+'", "'+response_by+'", "'+benificiary_name+'", "'+benificiary_img+'", "'+fathers_name+'", "'+mothers_name+'", "'+union_name+'", "'+union_name+'", "'+address+'", "'+gender+'", "'+age+'", "'+mobile_no+'", "'+nominee_name+'", "'+relation_with_nom+'", "'+nominee_img+'", "'+marital_status+'", "'+occupation_main+'", "'+occupation_2+'", "'+occupation_3+'", "'+location_gps+'")');
         }, errorCB, successCB);
     });
 });
 
+function get_input_value_of(inputID)
+{
+    return $(inputID).val();
+}
 
 function getLocationPos() {
     navigator.geolocation.getCurrentPosition(onSuccess, onError);
@@ -22,31 +48,4 @@ function onSuccess(position) {
 function onError(error) {
     alert('code: ' + error.code + '\n' +
             'message: ' + error.message + '\n');
-}
-
-function errorCB(tx, err) {
-    alert("Error processing SQL: " + err);
-}
-
-// Transaction success callback
-//
-function successCB() {
-    alert("success!");
-}
-
-
-function benificiaryList(tx, results) {
-    var benf_single = '';
-    var len = results.rows.length;
-    for (var i = 0; i < len; i++) {
-        benf_single += '<li><img class="text-center" src="'+results.rows.item(i).benificiary_img+'" alt=""> <span>Name: '+results.rows.item(i).benificiary_name +'</span></li>';
-    }   
-    $('#beneficiary-list').html(benf_single);
-}
-function populateBeneficiaryList() {
-    var db = window.openDatabase("oxfam_sims_dev", "1.0", "OxfamSIMS", 1000000);
-        db.transaction(function(tx) {
-            tx.executeSql('SELECT * FROM beneficiary_info ORDER BY b_id DESC', [], benificiaryList, errorCB);
-        }, errorCB, successCB);
-    	
 }
