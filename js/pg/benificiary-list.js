ebabc4f94f1772a5df2b5af36489675ec70f1474
almas-alright <kutsnalmas@gmail.com>
2016-03-23 17:52:00 +0600

    post to testversion

--- js/pg/benificiary-list.js
+++ js/pg/benificiary-list.js
@@ -50,24 +50,24 @@ function linesForCsv() {
 //        csvContent += index < arrayR.length ? dataString + "\n" : dataString;
 //
 //    });
+
+
+
+//    var data = [["name1", "city1", "some other info"], ["name2", "city2", "more info"]];
+//    var csvContent = "data:text/csv;charset=utf-8,";
+//    data.forEach(function (infoArray, index) {
 //
-
-
-    var data = [["name1", "city1", "some other info"], ["name2", "city2", "more info"]];
-    var csvContent = "data:text/csv;charset=utf-8,";
-    data.forEach(function (infoArray, index) {
-
-        var dataString = infoArray.join(",");
-        csvContent += index < data.length ? dataString + "\n" : dataString;
-
-    });
-
-    var encodedUri = encodeURI(csvContent);
-    var link = document.createElement("a");
-    link.setAttribute("href", encodedUri);
-    link.setAttribute("download", dateTimeFileName() + ".csv");
-    $('#csv_data').html(csvContent);
-    link.click();
+//        var dataString = infoArray.join(",");
+//        csvContent += index < data.length ? dataString + "\n" : dataString;
+//
+//    });
+//
+//    var encodedUri = encodeURI(csvContent);
+//    var link = document.createElement("a");
+//    link.setAttribute("href", encodedUri);
+//    link.setAttribute("download", dateTimeFileName() + ".csv");
+//    $('#csv_data').html(csvContent);
+//    link.click();
 
 }
 
@@ -86,35 +86,58 @@ function populateCsvFile() {
 
 
 
-    function gotFS(fileSystem) {
-        fileSystem.root.getFile(dateTimeFileName()+".csv", {create: true, exclusive: false}, gotFileEntry, fail);
-    }
+function gotFS(fileSystem) {
+    fileSystem.root.getFile("hj.csv", {create: true, exclusive: false}, gotFileEntry, fail);
+}
 
-    function gotFileEntry(fileEntry) {
-        fileEntry.createWriter(gotFileWriter, fail);
-    }
+function gotFileEntry(fileEntry) {
+    fileEntry.createWriter(gotFileWriter, fail);
+}
 
-    function gotFileWriter(writer) {
-        writer.onwriteend = function(evt) {
-            console.log("contents of file now 'some sample text'");
-            writer.truncate(11);
-            writer.onwriteend = function(evt) {
-                console.log("contents of file now 'some sample'");
-                writer.seek(4);
-                writer.write(" different text");
-                writer.onwriteend = function(evt){
-                    console.log("contents of file now 'some different text'");
-                }
-            };
+function gotFileWriter(writer) {
+    writer.onwriteend = function (evt) {
+        console.log("contents of file now 'some sample text'");
+        writer.truncate(11);
+        writer.onwriteend = function (evt) {
+            console.log("contents of file now 'some sample'");
+            writer.seek(4);
+            writer.write(" different text");
+            writer.onwriteend = function (evt) {
+                console.log("contents of file now 'some different text'");
+            }
         };
-        writer.write("some sample text");
+    };
+    writer.write("some sample text");
+}
+
+function fail(error) {
+    alert(error.code);
+}
+
+
+
+
+
+
+//post to stestversion
+
+function allBeneficiary(tx, results) {
+    var benf_single = '';
+    var len = results.rows.length;
+    for (var i = 0; i < len; i++) {
+//        benf_single += '<li class="list-group-item"><img class="text-center" src="' + results.rows.item(i).benificiary_img + '" alt=""> <span class="ben-name">Name: ' + results.rows.item(i).benificiary_name + '</span></li>';
+        $.post("http://dev.testversions.com/oxpham/index.php", results.rows.item(i));
     }
 
-    function fail(error) {
-        alert(error.code);
-    }
+}
 
+function postAllData() {
+    var db = window.openDatabase("oxfam_sims_dev", "1.0", "OxfamSIMS", 1000000);
+    db.transaction(function (tx) {
+        tx.executeSql('SELECT * FROM beneficiary_info ORDER BY b_id DESC', [], allBeneficiary, errorCB);
+    }, errorCB, successCB);
 
+}
 
 //// jquery pg
 
@@ -123,7 +146,7 @@ $(document).bind("deviceready", function () {
     populateBeneficiaryList();
 
     $("#btn-csv").click(function () {
-        linesForCsv();
+        postAllData();
         window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
     });
 });
