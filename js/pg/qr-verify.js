99a71aaf08f30bbc670889082579a9ba9bcbd163
almas-alright <kutsnalmas@gmail.com>
2016-04-18 13:15:50 +0600

    qr added

---
+++ js/pg/qr-verify.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var resultDiv;
+
+document.addEventListener("deviceready", init, false);
+function init() {
+    document.querySelector("#startScan").addEventListener("touchend", startScan, false);
+    resultDiv = document.querySelector("#results");
+}
+
+function startScan() {
+
+    cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                var s = "Result: " + result.text + "<br/>" +
+                        "Format: " + result.format + "<br/>" +
+                        "Cancelled: " + result.cancelled;
+                resultDiv.innerHTML = s;
+            },
+            function (error) {
+                alert("Scanning failed: " + error);
+            }
+    );
+
+}
