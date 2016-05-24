dff2cb0dd8b2b0d6f59b9ff487a18acd4d5ee584
almas-alright <kutsnalmas@gmail.com>
2016-05-24 15:33:07 +0600

    login added

---
+++ js/log.js
@@ -0,0 +1,52 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var w_width = $(window).width();
+var w_height = $(window).height();
+
+var user = "oxfam";
+var pass = "oxfam123";
+
+
+checkLog();
+
+$("#not-logged").width(w_width).height(w_height);
+
+$("#log-click").on('click', function () {
+    var user_field = $("#user").val();
+    var pass_field = $("#pass").val();
+
+    if ((user_field === user) && (pass_field === pass))
+    {
+        window.localStorage.setItem("logged", "yes");
+        checkLog();
+    } else {
+        window.localStorage.setItem("logged", "no");
+        checkLog();
+    }
+
+});
+
+$("#log-out").on('click', function () {
+    window.localStorage.setItem("logged", "no");
+    checkLog();
+    location.reload(); 
+});
+
+function checkLog()
+{
+    var flag = localStorage.getItem("logged")
+    if (flag === "no")
+    {
+        $("#logged").hide();
+    } else if (flag === "yes")
+    {
+        $("#not-logged").hide();
+        $("#logged").show();
+    }
+}
+
+
