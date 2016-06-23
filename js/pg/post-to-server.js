8419f0284a8c2a0aa73939e92fa10035e1654695
almas-alright <kutsnalmas@gmail.com>
2016-06-23 16:52:19 +0600

    fucking export

--- js/pg/post-to-server.js
+++ js/pg/post-to-server.js
@@ -28,49 +28,76 @@ function populateBeneficiaryList() {
     }, errorCB, successCB);
 }
 
+// list end
+
+
+
+
+
+
+function exportAll(tx, results) {
+    var benf_single = '';
+    var len = results.rows.length;
+    for (var i = 0; i < len; i++) {
+        $.ajax({
+            method: "POST",
+            url: "http://dev.testversions.com/oxfam/json-export.php",
+            data: {
+                project_id: results.rows.item(i).project_id,
+                office_id: results.rows.item(i).office_id,
+                beneficiary_id: results.rows.item(i).b_id,
+                group: results.rows.item(i).group_name,
+                name: results.rows.item(i).benificiary_name,
+                father: results.rows.item(i).fathers_name,
+                mother: results.rows.item(i).mothers_name,
+                union: results.rows.item(i).union_name,
+                ward: results.rows.item(i).word,
+                address: results.rows.item(i).address,
+                mobile_no: results.rows.item(i).mobile,
+                voter_id: results.rows.item(i).voter_id,
+                nominee_name: results.rows.item(i).nominee_name,
+                nominee_relation: results.rows.item(i).relation,
+                nominee_father: "Sariful Nominee Father",
+                nominee_mother: "Sariful Nominee Mother",
+                nominee_photo: results.rows.item(i).nominee_img,
+                lat: results.rows.item(i).location_gps,
+                lng: results.rows.item(i).location_gps,
+                national_id_image: "beneficiaries\/85814_nid-front.jpg",
+                national_id_image_back: "beneficiaries\/77953_nid-back.png",
+                beneficiary_photo: "beneficiaries\/22462_rahima.jpg",
+                updated_at: "",
+                created_at: "",
+                id: ""
+            }
+        }).done(function (msg) {
+
+            $("#result3").append(results.rows.item(i).b_id + '||');
+            $("#result").html(msg);
+        });
+    }
+}
+
+function retriveAndPost() {
+    var db = window.openDatabase("oxfam_sims_dev", "1.0", "OxfamSIMS", 1000000);
+    db.transaction(function (tx) {
+        tx.executeSql('SELECT * FROM beneficiary_info ORDER BY b_id DESC', [], exportAll, errorCB);
+    }, errorCB, successCB);
+
+}
+
 $(document).bind("deviceready", function () {
     populateBeneficiaryList();
-    var data = {
-        project_id: 1,
-        office_id: 2,
-        beneficiary_id: "BEN1223",
-        group: "Group",
-        name: "Shibly",
-        father: "Rahim Father",
-        mother: "Rahim Mother",
-        union: 3,
-        ward: "Poba",
-        address: "Dhaka Bangladesh",
-        mobile_no: "01613404299",
-        voter_id: "N82348723642367434",
-        nominee_name: "Nur Uddin",
-        nominee_relation: "Brother",
-        nominee_father: "Sariful Nominee Father",
-        nominee_mother: "Sariful Nominee Mother",
-        nominee_photo: "beneficiaries\/28498_nid-front.jpg",
-        lat: "24.094738167379017",
-        lng: "90.0023603439331",
-        national_id_image: "beneficiaries\/85814_nid-front.jpg",
-        national_id_image_back: "beneficiaries\/77953_nid-back.png",
-        beneficiary_photo: "beneficiaries\/22462_rahima.jpg",
-        updated_at: "",
-        created_at: "",
-        id: 10
-    };
 
-
-    $.ajax({
-        method: "POST",
-        url: "http://dev.testversions.com/oxfam/json-export.php",
-        data: data
-    }).done(function (msg) {
-//    alert( "Data Saved: " + msg );
-        $("#result").html(msg);
+    $("#btn-csv").click(function () {
+        retriveAndPost();
     });
-
-    $('.list-group-item').on('click', function () {        
+    
+    $("#btn-siz").on('touchend',function () {
+       alert($('li.list-group-item').length);
+    });
+    
+    $('li.list-group-item').click(function () {
         alert($(this).attr('data-bid'));
-        
     });
 
 });
